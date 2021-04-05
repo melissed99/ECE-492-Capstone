c0d78218fde71f5bd080cae589320c3f3d4c6249
Kathleen Pescador <kpescado@ualberta.ca>
2021-04-04 21:12:14 -0600

    added all code files for facial recognition

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Loaded with Raspbian with desktop and recommended software.
 ### Admin Password
 
 Password for root access is "sod".
+
+## Running the program 
+
+1. Run the face_rec/face_rec_updated.py
--- face_rec/saveImage.py
+++ face_rec/saveImage.py
@@ -2,14 +2,33 @@ import face_recognition
 import os #to iterate over directories
 import cv2
 from datetime import datetime
+from write_json import write_json
 
-def saveImage(image, name):
+
+def saveImage(image, name, check_action):
     filepath = "saved/" #where we want to save the images
     currentTime = datetime.now()
-    fn_time = currentTime.strftime("%Y-%m-%d %H.%M.%S") #includes time in filename
+    date = currentTime.strftime("%Y-%m-%d")
+    time = currentTime.strftime("%H:%M")
+    fn_time = currentTime.strftime("%Y-%m-%d %H.%M") #excludes seconds to avoid duplication
     fn_name = name + '_' + fn_time + '.jpg' #includes name and time in filename
     filename = filepath + fn_name #full filename of the image
 
-    isSaved = cv2.imwrite(filename, image) #save the frame where desired visitor has been located
+    data = {
+        'name': name,
+        'date': date,
+        'time': time,
+        'image_filename': fn_name,
+        'action': check_action
+        }
+    #save the frame where desired visitor has been located
+    #saves image to the 'saved' folder
+    isSaved = cv2.imwrite(filename, image)
+
+    #after successfully saving the image
+    #then save the information to a json file
     if isSaved:
         print("Visitor has been saved.")
+        write_json(data)
+    else:
+        print("Error with saving visitors data.")
